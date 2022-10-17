229cdeb79688bdd027117824bbef1733a8b2a268
Maria Tager <mariatager@Marias-MacBook-Air.local>
2022-10-17 11:44:37 +0200

    helpers.py add

--- helpers.py
+++ helpers.py
@@ -1,137 +1,38 @@
-from cmath import e
+"""Some helper functions for project 1."""
+import csv
 import numpy as np
 
 
-def e_vector(y, tx, w):
-    """Compute the error vector e used for other computations
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,D)
-        w: shape=(D,). The vector of model parameters.
+def load_csv_data(data_path, sub_sample=False):
+    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
 
-    Returns:
-           the value of the error vector e"""
+    # convert class labels from strings to binary (-1,1)
+    yb = np.ones(len(y))
+    yb[np.where(y == "b")] = -1
 
-    e = y - np.dot(tx, w)
-    return e
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        input_data = input_data[::50]
+        ids = ids[::50]
+
+    return yb, input_data, ids
 
 
-def compute_mse(y, tx, w):
-    """Calculate the loss using either MSE.
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,D)
-        w: shape=(2,). The vector of model parameters.
-
-    Returns:
-        the value of the loss (a scalar), corresponding to the input parameters w.
+def create_csv_submission(ids, y_pred, name):
     """
-
-    # compute loss by MSE
-    e = e_vector(y, tx, w)
-    mse = 1 / 2 * np.mean(e**2)
-    return mse
-
-
-def compute_gradient(y, tx, w):
-    """Computes the gradient at w.
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,D)
-        w: shape=(D, ). The vector of model parameters.
-
-    Returns:
-        An array of shape (D, ) (same shape as w), containing the gradient of the loss at w.
+    Creates an output file in .csv format for submission to Kaggle or AIcrowd
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
     """
-    e = e_vector(y, tx, w)
-    # compute gradient vector
-    gradient = -tx.T.dot(e) / len(e)
-    return gradient
-
-
-def compute_stoch_gradient(y, tx, w):
-    """Computes the stochastic gradient for few samples.
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,D)
-        w: shape=(D, ). The vector of model parameters.
-
-    Returns:
-        An array of shape (D, ) (same shape as w), containing the gradient of the loss at w.
-    """
-    e = e_vector(y, tx, w)
-    stoch_gradient = -tx.T.dot(e) / len(e)
-    return stoch_gradient
-
-
-def batch_iter(y, tx, batch_size=1, num_batches=1, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-    """
-    data_size = len(y)
-
-    if shuffle:
-        shuffle_indices = np.random.permutation(np.arange(data_size))
-        shuffled_y = y[shuffle_indices]
-        shuffled_tx = tx[shuffle_indices]
-    else:
-        shuffled_y = y
-        shuffled_tx = tx
-    for batch_num in range(num_batches):
-        start_index = batch_num * batch_size
-        end_index = min((batch_num + 1) * batch_size, data_size)
-        if start_index != end_index:
-            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
-
-
-def sigmoid(t):
-    """apply the sigmoid function on t.
-    Args:
-        t: scalar or numpy array
-
-    Returns:
-        sigmoid function (scalar or numpy array)
-    """
-    sigmoid = 1.0 / (1 + np.exp(-t))
-    return sigmoid
-
-
-def compute_log_loss(y, tx, w):
-    """Calculate the loss using negative log likelihood.
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,D)
-        w: shape=(D, ). The vector of model parameters.
-
-    Returns:
-        the value of the non-negative loss corresponding to the inputs.
-    """
-    logistic_function = sigmoid(tx.dot(w))
-    loss = y.T.dot(np.log(logistic_function)) + (1 - y).T.dot(
-        np.log(1 - logistic_function)
-    )
-    return np.squeeze(-loss).item()
-    # np.squeeze? np.item() ?
-
-
-def compute_log_gradient(y, tx, w):
-    """Computes the gradient at w using the logistic loss.
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,D)
-        w: shape=(D, ). The vector of model parameters.
-
-    Returns:
-        An array of shape (D, ) (same shape as w), containing the logistic gradient of the loss at w.
-    """
-    logistic_function = sigmoid(tx.dot(w))
-    log_gradient = tx.T.dot(logistic_function - y)
-    return log_gradient
+    with open(name, "w") as csvfile:
+        fieldnames = ["Id", "Prediction"]
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({"Id": int(r1), "Prediction": int(r2)})
